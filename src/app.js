3f7fc5ee2ac5000a09da21336201362f24b7f2ed
Robin Lack <lack@puzzle.ch>
2019-09-08 18:32:25 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {render} from "react-dom";
+
+const Pet = ({name, animal, breed}) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {},name),
+    React.createElement("h2", {},animal),
+    React.createElement("h2", {},breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {name: "Luna", animal: "Dog", breed:"Sheperd"}),
+    React.createElement(Pet, {name: "Fido", animal: "Dog", breed:"Labrador"}),
+    React.createElement(Pet, {name: "Pepper", animal: "Cat", breed:"Lion"})
+  );
+};
+
+render(React.createElement(App), document.getElementById("root"));
